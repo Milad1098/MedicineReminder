a7f65df8b974f942922caf640a8da7e12f373b24
Milad Mobaseri <140894198+Milad1098@users.noreply.github.com>
2026-05-21 21:06:41 +0330

    Update MainActivity.kt

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -1,17 +1,82 @@
 package com.example.test
 
-import android.app.Activity
 import android.os.Bundle
-import android.widget.TextView
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
-class MainActivity : Activity() {
+class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val text = TextView(this)
-        text.text = "Hello Milad 🔥"
+        setContent {
 
-        setContentView(text)
+            MaterialTheme {
+
+                Surface(
+                    modifier = Modifier.fillMaxSize()
+                ) {
+
+                    HomeScreen()
+
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun HomeScreen() {
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(24.dp),
+
+        verticalArrangement = Arrangement.Center,
+
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Card(
+            shape = RoundedCornerShape(28.dp)
+        ) {
+
+            Column(
+                modifier = Modifier.padding(32.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+
+                Text(
+                    text = "یادآور دارو",
+                    fontSize = 32.sp
+                )
+
+                Spacer(modifier = Modifier.height(12.dp))
+
+                Text(
+                    text = "داروهایت را فراموش نکن 💊",
+                    fontSize = 18.sp
+                )
+
+                Spacer(modifier = Modifier.height(24.dp))
+
+                Button(
+                    onClick = { }
+                ) {
+
+                    Text("افزودن دارو")
+
+                }
+            }
+        }
     }
 }
