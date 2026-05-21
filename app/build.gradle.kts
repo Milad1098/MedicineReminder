734390fc9a89a257772ac83b1d1cafcaedd0eda9
Milad Mobaseri <140894198+Milad1098@users.noreply.github.com>
2026-05-21 21:21:52 +0330

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,12 +42,13 @@ android {
 dependencies {
 
     implementation("androidx.core:core-ktx:1.13.1")
-
     implementation("androidx.activity:activity-compose:1.9.0")
-
     implementation("androidx.compose.ui:ui:1.6.8")
     implementation("androidx.compose.material3:material3:1.2.1")
     implementation("androidx.compose.ui:ui-tooling-preview:1.6.8")
-
+    implementation("androidx.compose.material3:material3")
+    implementation("androidx.navigation:navigation-compose:2.8.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.4")
+    implementation("androidx.compose.material:material-icons-extended")
     debugImplementation("androidx.compose.ui:ui-tooling:1.6.8")
 }
