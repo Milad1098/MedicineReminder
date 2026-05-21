4693681ceb7ac72124716e028d6ad350f6e4b541
Milad Mobaseri <140894198+Milad1098@users.noreply.github.com>
2026-05-21 20:57:51 +0330

    Update MainActivity.kt

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.test
 
 import android.app.Activity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val text = TextView(this)
+        text.text = "Hello Milad 🔥"
+
+        setContentView(text)
     }
 }
