4a0d6814e442c798ae44f909c5312950e0d51245
Milad Mobaseri <140894198+Milad1098@users.noreply.github.com>
2026-05-22 01:29:04 +0330

    Create Medicine.kt

---
+++ data/local/Medicine.kt
@@ -0,0 +1,19 @@
+package com.example.test.data.local
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "medicines")
+data class Medicine(
+
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+
+    val name: String,
+
+    val time: String,
+
+    val dosage: String,
+
+    val taken: Boolean = false
+)
