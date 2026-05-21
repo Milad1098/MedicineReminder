49750287cedf9f47f21db7355d231e77b4981bf6
Milad Mobaseri <140894198+Milad1098@users.noreply.github.com>
2026-05-21 21:06:07 +0330

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,16 +21,33 @@ android {
         }
     }
 
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+    buildFeatures {
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.14"
     }
 
     kotlinOptions {
         jvmTarget = "17"
     }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
 }
 
 dependencies {
+
     implementation("androidx.core:core-ktx:1.13.1")
+
+    implementation("androidx.activity:activity-compose:1.9.0")
+
+    implementation("androidx.compose.ui:ui:1.6.8")
+    implementation("androidx.compose.material3:material3:1.2.1")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.6.8")
+
+    debugImplementation("androidx.compose.ui:ui-tooling:1.6.8")
 }
