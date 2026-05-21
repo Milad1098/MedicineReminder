68cfe63895ac82da9e0049988cb98e796413f8dc
Milad Mobaseri <140894198+Milad1098@users.noreply.github.com>
2026-05-21 22:47:05 +0330

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,6 +61,24 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.7.7")
 
     implementation("androidx.compose.material:material-icons-extended")
-
+    // ViewModel
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.3")
+    
+    // Room Database
+    implementation("androidx.room:room-runtime:2.6.1")
+    implementation("androidx.room:room-ktx:2.6.1")
+    ksp("androidx.room:room-compiler:2.6.1")
+    
+    // Compose Navigation
+    implementation("androidx.navigation:navigation-compose:2.7.7")
+    
+    // Animation
+    implementation("androidx.compose.animation:animation")
+    
+    // System UI
+    implementation("com.google.accompanist:accompanist-systemuicontroller:0.34.0")
+    
+    // Ripple جدید
+    implementation("androidx.compose.material3:material3-window-size-class")
     debugImplementation("androidx.compose.ui:ui-tooling")
 }
