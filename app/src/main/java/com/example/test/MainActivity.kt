025bc77ecfa2f1820176c2698372f4cd5ba57ab1
Milad Mobaseri <140894198+Milad1098@users.noreply.github.com>
2026-05-22 22:01:03 +0330

    Update MainActivity.kt

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -1,35 +1,56 @@
 package com.example.test
 
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.os.PowerManager
+import android.provider.Settings
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.ui.unit.LayoutDirection
-import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.room.Room
 import com.example.test.data.local.AppDatabase
 import com.example.test.ui.screens.HomeScreen
+import com.example.test.ui.theme.TestTheme
 
 class MainActivity : ComponentActivity() {
 
-    private lateinit var db: AppDatabase
-
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
 
-        db = Room.databaseBuilder(
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+
+            val powerManager =
+                getSystemService(POWER_SERVICE) as PowerManager
+
+            if (
+                !powerManager.isIgnoringBatteryOptimizations(
+                    packageName
+                )
+            ) {
+
+                val intent = Intent(
+                    Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS,
+                    Uri.parse("package:$packageName")
+                )
+
+                startActivity(intent)
+            }
+        }
+
+        val db = Room.databaseBuilder(
             applicationContext,
             AppDatabase::class.java,
             "medicine_db"
         )
             .fallbackToDestructiveMigration()
+            .allowMainThreadQueries()
             .build()
 
         setContent {
 
-            CompositionLocalProvider(
-                LocalLayoutDirection provides LayoutDirection.Rtl
-            ) {
+            TestTheme {
 
                 HomeScreen(db = db)
             }
