80199de5acd7a9c49dde4e03b664388eec30a25c
Milad Mobaseri <140894198+Milad1098@users.noreply.github.com>
2026-05-22 14:34:46 +0330

    Create Font.kt

---
+++ app/src/main/java/com/example/test/ui/theme/Font.kt
@@ -0,0 +1,9 @@
+package com.example.test.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import com.example.test.R
+
+val Vazir = FontFamily(
+    Font(R.font.vazirmatn_regular)
+)
