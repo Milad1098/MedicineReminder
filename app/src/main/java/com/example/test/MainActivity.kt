c66c8d78030f42e48b64e93060e24c9660ce4fea
Milad Mobaseri <140894198+Milad1098@users.noreply.github.com>
2026-05-22 14:10:48 +0330

    Update MainActivity.kt

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -1,30 +1,25 @@
+```kotlin
 package com.example.test
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.*
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.platform.LocalLayoutDirection
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.LayoutDirection
-import androidx.room.Room
-import com.example.test.data.local.AppDatabase
-import com.example.test.data.local.Medicine
 import com.example.test.ui.screens.HomeScreen
-import kotlinx.coroutines.launch
 import com.example.test.utils.NotificationHelper
 
 class MainActivity : ComponentActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(
+        savedInstanceState: Bundle?
+    ) {
+
         super.onCreate(savedInstanceState)
 
-        val db = Room.databaseBuilder(
-            applicationContext,
-            AppDatabase::class.java,
-            "medicine_db"
-        ).build()
+        NotificationHelper
+            .createNotificationChannel(this)
 
         setContent {
 
@@ -32,44 +27,9 @@ class MainActivity : ComponentActivity() {
                 LocalLayoutDirection provides LayoutDirection.Rtl
             ) {
 
-                val dao = db.medicineDao()
-
-                var medicines by remember {
-                    mutableStateOf(listOf<Medicine>())
-                }
-
-                val scope = rememberCoroutineScope()
-
-                LaunchedEffect(true) {
-                    medicines = dao.getAll()
-                }
-
-                val vazir = FontFamily(
-                    Font(R.font.vazirmatn_regular)
-                )
-
-                HomeScreen(
-
-                    medicines = medicines,
-
-                    fontFamily = vazir,
-
-                    onAddMedicine = { name, time ->
-
-                        scope.launch {
-
-                            dao.insert(
-                                Medicine(
-                                    name = name,
-                                    time = time
-                                )
-                            )
-
-                            medicines = dao.getAll()
-                        }
-                    }
-                )
+                HomeScreen()
             }
         }
     }
 }
+```
