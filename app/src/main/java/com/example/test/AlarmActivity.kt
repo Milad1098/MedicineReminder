6baa1107949d2855dd836039442cfa65c8057be0
Milad Mobaseri <140894198+Milad1098@users.noreply.github.com>
2026-05-23 01:06:34 +0330

    Update AlarmActivity.kt

--- app/src/main/java/com/example/test/AlarmActivity.kt
+++ app/src/main/java/com/example/test/AlarmActivity.kt
@@ -1,19 +1,20 @@
 package com.example.test
 
-import android.media.MediaPlayer
-import android.os.Bundle
-import android.os.Vibrator
-import android.os.VibratorManager
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 class AlarmActivity : ComponentActivity() {
