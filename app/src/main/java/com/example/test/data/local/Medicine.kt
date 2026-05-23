f02c40baaee59028468a23d64a43393a8aaf42e5
Milad Mobaseri <140894198+Milad1098@users.noreply.github.com>
2026-05-23 23:56:20 +0330

    Update Medicine.kt

--- app/src/main/java/com/example/test/data/local/Medicine.kt
+++ app/src/main/java/com/example/test/data/local/Medicine.kt
@@ -5,11 +5,10 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "medicines")
 data class Medicine(
-
     @PrimaryKey(autoGenerate = true)
     val id: Int = 0,
-
     val name: String,
-
-    val time: String
+    val time: String,
+    val cycleHours: Int = 0,    // 0 = روزانه یکبار، 8 = هر ۸ ساعت
+    val dosesPerCycle: Int = 1  // تعداد وعده در سیکل
 )
