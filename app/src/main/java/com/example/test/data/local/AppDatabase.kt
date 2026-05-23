7b1b97d6081638e48c473dd920566264f8fbc10c
Milad Mobaseri <140894198+Milad1098@users.noreply.github.com>
2026-05-23 14:44:45 +0330

    Update AppDatabase.kt

--- app/src/main/java/com/example/test/data/local/AppDatabase.kt
+++ app/src/main/java/com/example/test/data/local/AppDatabase.kt
@@ -1,14 +1,42 @@
 package com.example.test.data.local
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(
     entities = [Medicine::class],
-    version = 2,
+    version = 1,
     exportSchema = false
 )
 abstract class AppDatabase : RoomDatabase() {
 
     abstract fun medicineDao(): MedicineDao
+
+    companion object {
+
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(
+            context: Context
+        ): AppDatabase {
+
+            return INSTANCE ?: synchronized(this) {
+
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "medicine_db"
+                )
+                    .fallbackToDestructiveMigration()
+                    .build()
+
+                INSTANCE = instance
+
+                instance
+            }
+        }
+    }
 }
