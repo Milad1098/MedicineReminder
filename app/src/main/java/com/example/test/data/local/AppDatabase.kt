44fdb7632ffa23e6f051a04b09677a0aea1d4f3a
Milad Mobaseri <140894198+Milad1098@users.noreply.github.com>
2026-05-23 23:56:49 +0330

    Update AppDatabase.kt

--- app/src/main/java/com/example/test/data/local/AppDatabase.kt
+++ app/src/main/java/com/example/test/data/local/AppDatabase.kt
@@ -7,7 +7,7 @@ import androidx.room.RoomDatabase
 
 @Database(
     entities = [Medicine::class],
-    version = 1,
+    version = 2,
     exportSchema = false
 )
 abstract class AppDatabase : RoomDatabase() {
@@ -15,27 +15,19 @@ abstract class AppDatabase : RoomDatabase() {
     abstract fun medicineDao(): MedicineDao
 
     companion object {
-
         @Volatile
         private var INSTANCE: AppDatabase? = null
 
-        fun getDatabase(
-            context: Context
-        ): AppDatabase {
-
+        fun getDatabase(context: Context): AppDatabase {
             return INSTANCE ?: synchronized(this) {
-
-                val instance = Room.databaseBuilder(
+                Room.databaseBuilder(
                     context.applicationContext,
                     AppDatabase::class.java,
                     "medicine_db"
                 )
-                    .fallbackToDestructiveMigration()
-                    .build()
-
-                INSTANCE = instance
-
-                instance
+                .fallbackToDestructiveMigration()
+                .build()
+                .also { INSTANCE = it }
             }
         }
     }
