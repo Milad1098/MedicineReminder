54613c7cdf42c6a865d93b632dbdad2d25508b1c
Milad Mobaseri <140894198+Milad1098@users.noreply.github.com>
2026-05-23 17:50:01 +0330

    Create AlarmScheduler.kt

---
+++ app/src/main/java/com/example/test/receiver/AlarmScheduler.kt
@@ -0,0 +1,84 @@
+package com.example.test.receiver
+
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import com.example.test.data.local.Medicine
+import java.util.Calendar
+
+object AlarmScheduler {
+
+    fun scheduleAlarm(
+        context: Context,
+        medicine: Medicine
+    ) {
+
+        val alarmManager =
+            context.getSystemService(
+                Context.ALARM_SERVICE
+            ) as AlarmManager
+
+        val parts = medicine.time.split(":")
+        val hour = parts[0].toInt()
+        val minute = parts[1].toInt()
+
+        val calendar = Calendar.getInstance().apply {
+            set(Calendar.HOUR_OF_DAY, hour)
+            set(Calendar.MINUTE, minute)
+            set(Calendar.SECOND, 0)
+
+            if (before(Calendar.getInstance())) {
+                add(Calendar.DAY_OF_MONTH, 1)
+            }
+        }
+
+        val intent = Intent(
+            context,
+            AlarmReceiver::class.java
+        ).apply {
+            putExtra("medicine_id", medicine.id)
+            putExtra("medicine_name", medicine.name)
+        }
+
+        val pendingIntent = PendingIntent.getBroadcast(
+            context,
+            medicine.id,
+            intent,
+            PendingIntent.FLAG_UPDATE_CURRENT or
+                    PendingIntent.FLAG_IMMUTABLE
+        )
+
+        alarmManager.setExactAndAllowWhileIdle(
+            AlarmManager.RTC_WAKEUP,
+            calendar.timeInMillis,
+            pendingIntent
+        )
+    }
+
+    fun cancelAlarm(
+        context: Context,
+        medicineId: Int
+    ) {
+
+        val alarmManager =
+            context.getSystemService(
+                Context.ALARM_SERVICE
+            ) as AlarmManager
+
+        val intent = Intent(
+            context,
+            AlarmReceiver::class.java
+        )
+
+        val pendingIntent = PendingIntent.getBroadcast(
+            context,
+            medicineId,
+            intent,
+            PendingIntent.FLAG_UPDATE_CURRENT or
+                    PendingIntent.FLAG_IMMUTABLE
+        )
+
+        alarmManager.cancel(pendingIntent)
+    }
+}
