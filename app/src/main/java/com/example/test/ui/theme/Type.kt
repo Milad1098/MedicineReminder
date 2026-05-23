9c63a70899adabbde79852489681da395ef78c86
Milad Mobaseri <140894198+Milad1098@users.noreply.github.com>
2026-05-23 16:52:25 +0330

    Create Type.kt

---
+++ app/src/main/java/com/example/test/ui/theme/Type.kt
@@ -0,0 +1,30 @@
+package com.example.test.ui.theme
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.example.test.R
+
+val Vazir = FontFamily(
+    Font(
+        resId = R.font.vazir,
+        weight = FontWeight.Normal
+    )
+)
+
+val Typography = Typography(
+
+    bodyLarge = TextStyle(
+        fontFamily = Vazir,
+        fontSize = 16.sp
+    ),
+
+    titleLarge = TextStyle(
+        fontFamily = Vazir,
+        fontSize = 22.sp,
+        fontWeight = FontWeight.Bold
+    )
+)
