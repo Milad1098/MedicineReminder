649622d8f1956f5a8b9903e4046e46921fc0a7c3
Milad Mobaseri <140894198+Milad1098@users.noreply.github.com>
2026-05-23 18:16:29 +0330

    Update MainActivity.kt

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -8,51 +8,34 @@ import android.os.PowerManager
 import android.provider.Settings
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.unit.LayoutDirection
 import com.example.test.ui.screens.HomeScreen
+import com.example.test.ui.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-
-            val powerManager =
-                getSystemService(POWER_SERVICE) as PowerManager
-
-            if (
-                !powerManager.isIgnoringBatteryOptimizations(
-                    packageName
-                )
-            ) {
-
+            val powerManager = getSystemService(POWER_SERVICE) as PowerManager
+            if (!powerManager.isIgnoringBatteryOptimizations(packageName)) {
                 val intent = Intent(
                     Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS,
                     Uri.parse("package:$packageName")
                 )
-
                 startActivity(intent)
             }
         }
 
         setContent {
-
             CompositionLocalProvider(
                 LocalLayoutDirection provides LayoutDirection.Rtl
             ) {
-
-                MaterialTheme {
-
-                    Surface {
-
-                        HomeScreen()
-                    }
+                AppTheme {
+                    HomeScreen()
                 }
             }
         }
