4ff4153089c702cc983e27a21dad178105ed7636
Milad Mobaseri <140894198+Milad1098@users.noreply.github.com>
2026-05-23 17:42:16 +0330

    Update MedicineDao.kt

--- app/src/main/java/com/example/test/data/local/MedicineDao.kt
+++ app/src/main/java/com/example/test/data/local/MedicineDao.kt
@@ -10,23 +10,18 @@ import kotlinx.coroutines.flow.Flow
 @Dao
 interface MedicineDao {
 
-    @Query(
-        "SELECT * FROM medicines ORDER BY id DESC"
-    )
+    @Query("SELECT * FROM medicines ORDER BY id DESC")
     fun getAllMedicines(): Flow<List<Medicine>>
 
+    @Query("SELECT * FROM medicines")
+    suspend fun getAllMedicinesSync(): List<Medicine>
+
     @Insert
-    suspend fun insert(
-        medicine: Medicine
-    ): Long
+    suspend fun insert(medicine: Medicine): Long
 
     @Update
-    suspend fun update(
-        medicine: Medicine
-    )
+    suspend fun update(medicine: Medicine)
 
     @Delete
-    suspend fun delete(
-        medicine: Medicine
-    )
+    suspend fun delete(medicine: Medicine)
 }
