447e6655aa4e77ed9bf2f9acfec80d37954206a9
Milad Mobaseri <140894198+Milad1098@users.noreply.github.com>
2026-05-23 13:44:56 +0330

    Update Medicine.kt

--- app/src/main/java/com/example/test/data/local/Medicine.kt
+++ app/src/main/java/com/example/test/data/local/Medicine.kt
@@ -3,13 +3,12 @@ package com.example.test.data.local
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity
+@Entity(tableName = "medicines")
 data class Medicine(
 
     @PrimaryKey(autoGenerate = true)
     val id: Int = 0,
 
     val name: String,
-
     val time: String
 )
