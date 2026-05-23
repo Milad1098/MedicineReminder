19580c8f9315335f75fc8fe67b687b50f7dfe2a5
Milad Mobaseri <140894198+Milad1098@users.noreply.github.com>
2026-05-23 17:42:27 +0330

    Update BootReceiver.kt

--- app/src/main/java/com/example/test/receiver/BootReceiver.kt
+++ app/src/main/java/com/example/test/receiver/BootReceiver.kt
@@ -3,13 +3,19 @@ package com.example.test.receiver
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import com.example.test.data.local.AppDatabase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class BootReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent) {
+
         if (intent.action == Intent.ACTION_BOOT_COMPLETED) {
-    
+
             val scope = CoroutineScope(Dispatchers.IO)
+
             scope.launch {
                 val db = AppDatabase.getDatabase(context)
                 val medicines = db.medicineDao().getAllMedicinesSync()
