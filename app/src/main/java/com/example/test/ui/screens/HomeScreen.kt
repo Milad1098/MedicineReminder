48fc7a955d27482d36123c53527b6ae3a6b66f18
Milad Mobaseri <140894198+Milad1098@users.noreply.github.com>
2026-05-23 14:21:00 +0330

    Update HomeScreen.kt

--- app/src/main/java/com/example/test/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/test/ui/screens/HomeScreen.kt
@@ -1,41 +1,49 @@
 package com.example.test.ui.screens
 
-import androidx.compose.foundation.background
+import android.widget.Toast
+
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
-import com.example.test.alarm.AlarmScheduler
-import com.example.test.data.local.AppDatabase
+
+import androidx.lifecycle.viewmodel.compose.viewModel
+
 import com.example.test.data.local.Medicine
+import com.example.test.receiver.AlarmScheduler
+
+import com.example.test.ui.components.AddMedicineDialog
 import com.example.test.ui.components.EmptyState
 import com.example.test.ui.components.HeaderSection
 import com.example.test.ui.components.MedicineCard
-import com.example.test.ui.theme.Vazir
-import kotlinx.coroutines.launch
+
+import com.example.test.viewmodel.MedicineViewModel
 
 @Composable
 fun HomeScreen(
